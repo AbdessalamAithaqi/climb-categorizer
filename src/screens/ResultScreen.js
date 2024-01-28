8b4e8d810cba2287b0ada0079b940e02f9600d3a
AbdessalamAithaqi <abdessalam.aithaqi@gmail.com>
2024-01-28 04:47:11 -0500

    Update dependencies and add image gallery to ResultScreen

--- src/screens/ResultScreen.js
+++ src/screens/ResultScreen.js
@@ -1,31 +1,59 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Image } from 'react-native';
 
 const ResultScreen = ({ navigation }) => {
+    // Array of image sources
+    const images = [
+        require('../styles/images/templatePic1.jpg'), // Update the path to your local images
+        require('../styles/images/templatePic2.jpg'),
+        require('../styles/images/templatePic3.jpg'),
+
+        // ... more images
+    ];
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Result Screen</Text>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => navigation.navigate('Error')}
-            >
-                <Text style={styles.text}>Go to Error</Text>
-            </TouchableOpacity>
+        <View style={{ flex: 1 }}>
+            <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+                {images.map((image, index) => (
+                    <Image key={index} source={image} style={styles.image} />
+                ))}
+            </ScrollView>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => navigation.navigate('Error')}
+                >
+                    <Text style={styles.text}>Go to Error</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
 
 // Define your styles here
 const styles = StyleSheet.create({
+    scrollViewContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 300, // Adjust as needed
+        height: 300, // Adjust as needed
+        marginVertical: 10,
+    },
+    buttonContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 20,
+    },
     button: {
-        backgroundColor: 'orange', // Example color
-        borderRadius: 20,          // Example border radius
-        padding: 10,               // Example padding
-        marginTop: 20,             // Example margin
+        backgroundColor: 'orange',
+        borderRadius: 20,
+        padding: 10,
     },
     text: {
-        color: 'white',            // Example text color
-        fontSize: 16,              // Example font size
+        color: 'white',
+        fontSize: 16,
     },
 });
 
