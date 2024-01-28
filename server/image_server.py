857b2eb4838652ee372b9ea69f335a83d335750c
JeffBrin <bringolfj@hotmail.com>
2024-01-28 05:37:52 -0500

    Added instructions to run server

--- server/image_server.py
+++ server/image_server.py
@@ -14,7 +14,7 @@ class MyServer(BaseHTTPRequestHandler):
         print("POST")
         if self.path == "/upload":
             self.send_response(200)
-            self.send_header('Content-type', 'text/jpeg')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
             post_data = self.rfile.read(content_length) 
