67bc36da6df8e40a737b4d7365744c140c6950dc
YuriSorice <yuri.sorice@mail.mcgill.ca>
2024-01-28 11:02:56 -0500

    changed color palette for visibility

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,9 +5,10 @@
 // Import necessary modules from react and react-native
 import React from "react";
 import { StyleSheet, Animated, View, Dimensions } from "react-native";
+import colorPalette from "../styles/GlobalStyles.js";
 
 // Get the screen width
-const { width } = Dimensions.get('screen');
+const { width } = Dimensions.get("screen");
 
 /**
  * The Pagination component.
@@ -18,40 +19,40 @@ const { width } = Dimensions.get('screen');
  * @returns {React.Component} The Pagination component.
  */
 const Pagination = ({ data, scrollX }) => (
-    <View style={styles.container}>
-        {data.map((_, i) => {
-            const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
-            const dotWidth = scrollX.interpolate({
-                inputRange,
-                outputRange: [12, 30, 12],
-                extrapolate: 'clamp',
-            });
+  <View style={styles.container}>
+    {data.map((_, i) => {
+      const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+      const dotWidth = scrollX.interpolate({
+        inputRange,
+        outputRange: [12, 30, 12],
+        extrapolate: "clamp",
+      });
 
-            return (
-                <Animated.View
-                    style={[styles.dot, { width: dotWidth }]}
-                    key={i.toString()}
-                />
-            );
-        })}
-    </View>
+      return (
+        <Animated.View
+          style={[styles.dot, { width: dotWidth }]}
+          key={i.toString()}
+        />
+      );
+    })}
+  </View>
 );
 
 export default Pagination;
 
 const styles = StyleSheet.create({
-    container: {
-        position: 'absolute',
-        bottom: -20, // Position adjusted to below the slider
-        flexDirection: 'row',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    dot: {
-        height: 12,
-        borderRadius: 6,
-        marginHorizontal: 3,
-        backgroundColor: '#ccc',
-    }
+  container: {
+    position: "absolute",
+    bottom: -20, // Position adjusted to below the slider
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dot: {
+    height: 12,
+    borderRadius: 6,
+    marginHorizontal: 3,
+    backgroundColor: 'black',
+  },
 });
