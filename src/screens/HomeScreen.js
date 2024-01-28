a20396c95be4931607780d782b7134b479c3fe38
YuriSorice <yuri.sorice@mail.mcgill.ca>
2024-01-28 11:51:34 -0500

    fixed error redirect

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Camera } from 'expo-camera';
-import Slides from '../data/Slides';
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import Slides from "../data/Slides";
 
-SERVER_URL = "http://ec2-18-205-36-146.compute-1.amazonaws.com"
-POST_ENDPOINT = "upload"
-GET_ENDPOINT = "next"
-
+SERVER_URL = "http://ec2-18-205-36-146.compute-1.amazonaws.com";
+POST_ENDPOINT = "upload";
+GET_ENDPOINT = "next";
 
 const HomeScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
@@ -16,7 +15,7 @@ const HomeScreen = ({ navigation }) => {
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
@@ -34,46 +33,49 @@ const HomeScreen = ({ navigation }) => {
       const options = { quality: 1, base64: true };
       const data = await cameraRef.current.takePictureAsync(options);
 
-      console.log("TOOK PIC")
-      let response
+      console.log("TOOK PIC");
+      let response;
       try {
-        response = await (await fetch(`${SERVER_URL}/${POST_ENDPOINT}`, {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'image/jpeg'
-          },
-          body: data.base64
-        })).json()
-      }
-      catch (e) {
-        console.log(e)
+        response = await (
+          await fetch(`${SERVER_URL}/${POST_ENDPOINT}`, {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "image/jpeg",
+            },
+            body: data.base64,
+          })
+        ).json();
+      } catch (e) {
+        navigation.navigate("Error");
       }
 
-      console.log(response)
+      console.log(response);
 
       // Handle the taken picture, you can send it to the server, save it locally, etc.
-      token = response['token']
-      colors = response['colors']
-      console.log(token)
-      console.log(colors)
+      token = response["token"];
+      colors = response["colors"];
+      console.log(token);
+      console.log(colors);
 
       id = 1;
       while (Slides.length > 0) {
-        Slides.pop()
-        console.log("popped")
+        Slides.pop();
+        console.log("popped");
       }
       for (let id = 0; id < colors.length; id++) {
-        color = colors[id]
-        console.log(color)
-        let data = await fetch(`${SERVER_URL}/${GET_ENDPOINT}?color=${color}&token=${token}`)
-        data = await data.json()
-        const img = data.image
+        color = colors[id];
+        console.log(color);
+        let data = await fetch(
+          `${SERVER_URL}/${GET_ENDPOINT}?color=${color}&token=${token}`
+        );
+        data = await data.json();
+        const img = data.image;
         Slides.push({
           id: id + 1,
           img: img,
           color: color,
-        })
+        });
       }
     }
   };
@@ -98,7 +100,7 @@ const HomeScreen = ({ navigation }) => {
             style={styles.button}
             onPress={async () => {
               await takePicture();
-              navigation.navigate('Result')
+              navigation.navigate("Result");
             }}
           >
             <Text style={styles.text}>Capture</Text>
@@ -109,7 +111,6 @@ const HomeScreen = ({ navigation }) => {
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -119,24 +120,24 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
+    backgroundColor: "transparent",
+    flexDirection: "row",
     margin: 20,
     marginBottom: 60, // Increased bottom margin
-    justifyContent: 'space-around',
-    alignItems: 'flex-end',
+    justifyContent: "space-around",
+    alignItems: "flex-end",
   },
   button: {
-    backgroundColor: 'orange',
+    backgroundColor: "orange",
     borderRadius: 20,
     padding: 10,
     paddingHorizontal: 20,
-    elevation: 2,  // Add shadow for Android (optional)
+    elevation: 2, // Add shadow for Android (optional)
   },
   text: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
 
