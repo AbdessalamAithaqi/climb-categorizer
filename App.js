b5a9a6e0e899722c26142e5316657562b8ec4be9
AbdessalamAithaqi <abdessalam.aithaqi@gmail.com>
2024-01-28 09:26:42 -0500

    Add comments and export App component as default

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
+/**
+ * @fileoverview This module provides the main entry point for the application.
+ */
+
+// Import necessary modules from react and react-navigation
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 
-// Wraps the app in a NavigationContainer
+/**
+ * The main App component.
+ * It wraps the main component in a NavigationContainer.
+ * The NavigationContainer is responsible for managing the app state and linking the top-level navigator to the app environment.
+ * @returns {React.Component} The main application component.
+ */
 const App = () => {
     return (
         <NavigationContainer>
+            {/* AppNavigator is the top-level navigator, defined in './src/navigation/AppNavigator' */}
             <AppNavigator />
         </NavigationContainer>
     );
 };
 
+// Export the App component as the default export
 export default App;
