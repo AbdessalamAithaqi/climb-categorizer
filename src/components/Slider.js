c650c845b009e2b144bb889d9d0353c7abc8188b
AbdessalamAithaqi <abdessalam.aithaqi@gmail.com>
2024-01-28 09:50:36 -0500

    Document and optimize Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,35 +1,36 @@
-import { StyleSheet, Text, View, Animated, FlatList } from "react-native";
+/**
+ * @fileoverview This module provides the Slider component for the application.
+ */
+
+// Import necessary modules from react and react-native
 import React, { useRef } from "react";
+import { View, Animated, FlatList } from "react-native";
 import Slides from "../data/Slides";
 import SlideItem from "./SlideItem";
 import Pagination from "./Pagination";
 
+/**
+ * The Slider component.
+ * It displays a list of slides with pagination.
+ * @returns {React.Component} The Slider component.
+ */
 const Slider = () => {
+    // Reference to the Animated.Value for the x offset of the scroll view
     const scrollX = useRef(new Animated.Value(0)).current;
 
-    const handleOnScroll = (event) => {
-        Animated.event(
-            [
-                {
-                    nativeEvent: {
-                        contentOffset: { x: scrollX }
-                    },
-                },
-            ],
-            {
-                useNativeDriver: false,
-            }
-        )(event);
-    }
     return (
         <View>
-            <FlatList data={Slides}
+            <FlatList
+                data={Slides}
                 renderItem={({ item }) => <SlideItem item={item} />}
                 horizontal
                 pagingEnabled
                 snapToAlignment="center"
                 showsHorizontalScrollIndicator={false}
-                onScroll={handleOnScroll}
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+                    { useNativeDriver: false }
+                )}
             />
             <Pagination data={Slides} scrollX={scrollX} />
         </View>
@@ -37,5 +38,3 @@ const Slider = () => {
 }
 
 export default Slider;
-
-const styles = StyleSheet.create({});
