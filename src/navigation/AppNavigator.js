efea1222896128a2450d022dd7b2b86aade2e4df
AbdessalamAithaqi <abdessalam.aithaqi@gmail.com>
2024-01-27 22:57:59 -0500

    Add slide animation to screen transition

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,15 +1,62 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Animated, Easing } from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import ResultScreen from '../screens/ResultScreen';
 
-// Creates the navigation stack for the app
 const Stack = createNativeStackNavigator();
 
-// Defines the navigation and screens for the app
+const forSlide = ({ current, next, inverted, layouts: { screen } }) => {
+  const progress = Animated.add(
+    current.progress.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+      extrapolate: 'clamp',
+    }),
+    next
+      ? next.progress.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1],
+        extrapolate: 'clamp',
+      })
+      : 0
+  );
+
+  return {
+    cardStyle: {
+      transform: [
+        {
+          translateY: Animated.multiply(
+            progress.interpolate({
+              inputRange: [0, 1, 2],
+              outputRange: [
+                screen.height, // Start from the bottom
+                0, // Move to the top
+                screen.height * -1, // Move up above the screen
+              ],
+              extrapolate: 'clamp',
+            }),
+            inverted
+          ),
+        },
+      ],
+    },
+  };
+};
+
 const AppNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: true,
+        gestureDirection: 'vertical',
+        transitionSpec: {
+          open: { animation: 'timing', config: { duration: 500, easing: Easing.inOut(Easing.ease) } },
+          close: { animation: 'timing', config: { duration: 500, easing: Easing.inOut(Easing.ease) } },
+        },
+        cardStyleInterpolator: forSlide,
+      }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Result" component={ResultScreen} />
     </Stack.Navigator>
