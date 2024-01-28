0abc25e96177e3d3f4698973fa14b83ab1211c43
Jeffrey Bringolf <65859589+jeffbrin@users.noreply.github.com>
2024-01-28 11:09:37 -0500

    Backend development (#10)
    * Server image processing (#3)
    
    * Image processing fully functioning
    
    * Commiting progress before I do something stupid
    
    * Returning bytes
    
    * Server is working, model is brutal
    
    * Removed images
    
    * Added instructions to run server
    
    * Updated instructions
    
    * deleted images
    
    * Fixed model (#4)
    
    * updated logo
    
    * Remove unused code and imports in ResultScreen.js
    
    * Add comments and export App component as default
    
    * Delete unnecessary files
    
    * Add documentation to ResultScreen
    
    * Document ErrorScreen
    
    * Document nd optimize app navigator
    
    * Document and optimize Slider.js
    
    * Document and optimize SlideItem.js
    
    * Document and clean Pagination.js
    
    * Integrate Application code to the main (#7)
    
    * add default .gitignore for react native
    
    * add default react native file structure
    
    * Add react-native dependency to package.json
    
    * Update dependencies in package.json
    
    * Add AppNavigator component
    
    * Add HomeScreen component
    
    * Add navigation and result screen components
    
    * Add .expo folder and devices.json file
    
    * Add external Node.js modules
    
    * Add framer-motion library and its dependencies
    
    * Add slide animation to screen transition
    
    * Add Camera component for taking pictures
    
    * Add react-native-camera dependency
    
    * Add temporary image file
    
    * Add camera functionality to HomeScreen
    
    * Update button styles and functionality in HomeScreen.js
    
    * Hide header for HomeScreen in AppNavigator
    
    * Add ErrorScreen component and navigation to ResultScreen
    
    * Delete unused Camera component and add new template images
    
    * Update dependencies and add image gallery to ResultScreen
    
    * Move images to assets folder
    
    * Add Pagination and Slider components
    
    * Import backend logic to integrate the logic to the view (#5)
    
    * Server image processing (#3)
    
    * Image processing fully functioning
    
    * Commiting progress before I do something stupid
    
    * Returning bytes
    
    * Server is working, model is brutal
    
    * Removed images
    
    * Added instructions to run server
    
    * Updated instructions
    
    * deleted images
    
    * Fixed model (#4)
    
    ---------
    
    Co-authored-by: Jeffrey Bringolf <65859589+jeffbrin@users.noreply.github.com>
    
    * updated logo
    
    * Remove unused code and imports in ResultScreen.js
    
    * Add comments and export App component as default
    
    * Delete unnecessary files
    
    * Add documentation to ResultScreen
    
    * Document ErrorScreen
    
    * Document nd optimize app navigator
    
    * Document and optimize Slider.js
    
    * Document and optimize SlideItem.js
    
    * Document and clean Pagination.js
    
    ---------
    
    Co-authored-by: Jeffrey Bringolf <65859589+jeffbrin@users.noreply.github.com>
    Co-authored-by: YuriSorice <yuri.sorice@mail.mcgill.ca>
    
    * changed color palette for visibility
    
    ---------
    
    Co-authored-by: YuriSorice <yuri.sorice@mail.mcgill.ca>
    Co-authored-by: AbdessalamAithaqi <abdessalam.aithaqi@gmail.com>
    Co-authored-by: Abdel <78218029+AbdessalamAithaqi@users.noreply.github.com>

--- src/screens/ResultScreen.js
+++ src/screens/ResultScreen.js
@@ -1,54 +1,38 @@
+/**
+ * @fileoverview This module provides the ResultScreen component for the application.
+ */
+
+// Import necessary modules from react, react-native and react-native-safe-area-context
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Slider from '../components/Slider';
 
-const ResultScreen = ({ navigation }) => {
+/**
+ * The ResultScreen component.
+ * It wraps the Slider component in a SafeAreaView.
+ * @returns {React.Component} The ResultScreen component.
+ */
+const ResultScreen = () => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            {/* <ScrollView contentContainerStyle={styles.scrollViewContainer}>
-                {images.map((image, index) => (
-                    <Image key={index} source={image} style={styles.image} />
-                ))}
-            </ScrollView>
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => navigation.navigate('Error')}
-                >
-                    <Text style={styles.text}>Go to Error</Text>
-                </TouchableOpacity>
-            </View> */}
-
             <Slider />
         </SafeAreaView>
     );
 };
 
-// Define your styles here
+/**
+ * Stylesheet for the ResultScreen component.
+ */
 const styles = StyleSheet.create({
     scrollViewContainer: {
         alignItems: 'center',
         justifyContent: 'center',
     },
     image: {
-        width: 300, // Adjust as needed
-        height: 300, // Adjust as needed
+        width: 300,
+        height: 300, 
         marginVertical: 10,
-    },
-    buttonContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        margin: 20,
-    },
-    button: {
-        backgroundColor: 'orange',
-        borderRadius: 20,
-        padding: 10,
-    },
-    text: {
-        color: 'white',
-        fontSize: 16,
     },
 });
 
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,11 @@
+const colorPalette = {
+    primary: '#2E86AB',
+    secondary:'#EFA00B',
+    accent:'#D65108',
+    background: '#591F0A',
+    text: '#B5CBB7',
+    white:'#f7fbfc',
+    black:'#000203',
+}
+
+export default colorPalette;
